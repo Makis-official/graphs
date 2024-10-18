7159d6bd7749757fc35156e27f95165613222132
Makis-official <nesterenkomakar@yandex.ru>
2024-10-18 09:27:34 +0300

    изменение цвета узла

--- src/app/components/cytoscape/cytoscape.component.ts
+++ src/app/components/cytoscape/cytoscape.component.ts
@@ -1,189 +1,282 @@
 import { Component, ElementRef, OnInit } from '@angular/core';
 import cytoscape from 'cytoscape';
 import edgehandles from 'cytoscape-edgehandles';
-import { HttpClient } from '@angular/common/http';
 import oneGraph from '../../jsonFiles/config1.json';
 import bigGraph from '../../jsonFiles/config2.json';
 import { ActivatedRoute, Router } from "@angular/router";
 
 
 @Component({
-    selector: 'app-cytoscape',
-    templateUrl: './cytoscape.component.html',
-    styleUrls: ['./cytoscape.component.less'],
+  selector: 'app-cytoscape',
+  templateUrl: './cytoscape.component.html',
+  styleUrls: ['./cytoscape.component.less'],
 })
 export class CytoscapeComponent implements OnInit {
+  private cy: any;
+  public id: number = Date.now();
+  public selectBlock: any = undefined;
+  public drawMode: boolean = false;
+  public typeBlock: string = '';
+  public openModal: boolean = false;
+  public selectedColor: string = '';
 
-    private cy: any;
-    public id: number = Date.now();
-    public selectBlock: any = undefined;
-    public drawMode: boolean = false;
-    public typeBlock: string = '';
-    public openModal: boolean = false;
-    public selectedColor: string = '';
+  constructor(
+    private el: ElementRef,
+    private _router: Router,
+    private _route: ActivatedRoute
+  ) {}
 
-    constructor(
-        private el: ElementRef,
-        private _router: Router,
-        private _route: ActivatedRoute,
-    ) { }
+  public ngOnInit() {
+    const id = this._route.snapshot.params['id'];
 
-    public ngOnInit() {
-        const id = this._route.snapshot.params['id'];
+    cytoscape.use(edgehandles);
 
-        cytoscape.use( edgehandles );
+    this.cy = cytoscape({
+      container: this.el.nativeElement.querySelector('#cy'),
 
-        this.cy = cytoscape({
-            container: this.el.nativeElement.querySelector('#cy'),
+      elements: [],
+      style: [],
+    });
 
-            elements: [],
-            style: [],
-        });
+    // обращаемся к пользовательскому интерфесу для работы с узлами
+    let eh = this.cy.edgehandles();
+    let graphConfig;
 
-        // обращаемся к пользовательскому интерфесу для работы с узлами
-        let eh = this.cy.edgehandles();
-        let graphConfig;
+    if (Number(id) === 1) {
+      graphConfig = oneGraph;
+    } else {
+      graphConfig = bigGraph;
+    }
 
-        if (Number(id) === 1) {
-            graphConfig = oneGraph;
-        } else {
-            graphConfig = bigGraph;
+    graphConfig.elements.nodes.forEach((el) => {
+      this.cy.add({
+        group: 'nodes',
+        data: el.data,
+        position: el.position,
+      });
+    });
+
+    graphConfig.elements.edges.forEach((el) => {
+      this.cy.add({
+        group: 'edges',
+        data: el.data,
+        position: el.position,
+      });
+    });
+
+    this.cy.style(graphConfig.style);
+
+    this.el.nativeElement
+      .querySelector('#draw-on')
+      .addEventListener('click', () => {
+        eh.enableDrawMode();
+      });
+
+    this.el.nativeElement
+      .querySelector('#draw-off')
+      .addEventListener('click', function () {
+        eh.disableDrawMode();
+      });
+
+    this.cy.on('click', 'node', (evt: any) => {
+      this.previewSelect(evt);
+    });
+
+    this.cy.on('dblclick', 'edge', (evt: any) => {
+      this.removeSelected(evt);
+      this.closePreview();
+    });
+
+    this.el.nativeElement.addEventListener('dragstart', (event: any) => {
+      this.typeBlock = event.target.id;
+    });
+
+    this.el.nativeElement.addEventListener('dragover', (event: any) => {
+      event.preventDefault();
+    });
+
+    this.el.nativeElement.addEventListener('drop', (event: any) => {
+      event.preventDefault();
+      console.log(event);
+      const rect = this.el.nativeElement.getBoundingClientRect();
+      const pos = {
+        x: event.clientX - rect.left,
+        y: event.clientY - rect.top,
+      };
+      this.addNode(
+        pos.x,
+        pos.y,
+        `${(this.id += 1)}`,
+        {
+          name: 'Nikita',
+          date: Date.now().toString(),
+        },
+        this.typeBlock
+      );
+    });
+  }
+
+  public previewSelect(evt: any) {
+    this.selectBlock = evt;
+    // console.log(this.selectBlock.target['_private'].data.type);
+  }
+
+  public closePreview() {
+    this.selectBlock = undefined;
+  }
+
+  public selectColor(color: string) {
+    this.selectedColor = color;
+
+    return color;
+  }
+
+  public saveNewStyle() {
+    let id = this.selectBlock.target['_private'].data.id;
+
+    switch (this.selectedColor) {
+      case 'yellow':
+        if (
+          !this.cy
+            .style()
+            .json()
+            .find((el: any) => el.selector === '.yellow')
+        ) {
+          this.cy
+            .style()
+            .selector(`.yellow`)
+            .style({
+              'background-color': 'yellow',
+            })
+            .update();
         }
 
+        this.cy.getElementById(id).classes('yellow');
+        break;
 
-        graphConfig.elements.nodes.forEach(el => {
-            this.cy.add({
-                group: 'nodes',
-                data: el.data,
-                position: el.position,
-            });
-        })
-
-        graphConfig.elements.edges.forEach(el => {
-            this.cy.add({
-                group: 'edges',
-                data: el.data,
-                position: el.position,
-            });
-        })
-
-        this.cy.style(graphConfig.style);
-
-
-
-        this.el.nativeElement.querySelector('#draw-on').addEventListener('click', () => {
-            eh.enableDrawMode();
-        });
-
-        this.el.nativeElement.querySelector('#draw-off').addEventListener('click', function () {
-            eh.disableDrawMode();
-        });
-
-        this.cy.on('click', 'node', (evt: any) => {
-            this.previewSelect(evt);
-        });
-
-        this.cy.on('dblclick', 'edge', (evt: any) => {
-            this.removeSelected(evt);
-            this.closePreview();
-        });
-
-        this.el.nativeElement.addEventListener('dragstart', (event: any) => {
-            this.typeBlock = event.target.id;
-        });
-
-        this.el.nativeElement.addEventListener('dragover', (event: any) => {
-            event.preventDefault();
-        });
-
-        this.el.nativeElement.addEventListener('drop', (event: any) => {
-            event.preventDefault();
-            console.log(event)
-            const rect = this.el.nativeElement.getBoundingClientRect();
-            const pos = {
-                x: event.clientX - rect.left,
-                y: event.clientY - rect.top
-            };
-            this.addNode(pos.x, pos.y, `${this.id += 1}`, {
-                    name: 'Nikita',
-                    date: Date.now().toString(),
-                },
-                this.typeBlock,
-            );
-        });
-    }
-
-    public previewSelect(evt: any) {
-        this.selectBlock = evt;
-        // console.log(this.selectBlock.target['_private'].data.type);
-    }
-
-    public closePreview() {
-        this.selectBlock = undefined;
-    }
-
-    public selectColor(color: string) {
-        this.selectedColor = color;
-
-        return color;
-    }
-
-    public saveNewStyle() {
-        let id = this.selectBlock.target['_private'].data.id;
-
-        switch (this.selectedColor) {
-
-            case 'yellow':
-
-                if (!this.cy.style().json().find((el: any) => el.selector === '.yellow')) {
-                    this.cy.style()
-                        .selector(`.yellow`)
-                        .style({
-                            'background-color': 'yellow',
-                        }).update();
-                }
-
-                this.cy.getElementById(id).addClass('yellow');
-                break;
-
-            case 'green':
-                if (!this.cy.style().json().find((el: any) => el.selector === '.green')) {
-                    this.cy.style()
-                        .selector(`.green`)
-                        .style({
-                            'background-color': 'green',
-                        }).update();
-                }
-
-                this.cy.getElementById(id).addClass('green');
-
-                break;
+      case 'green':
+        if (
+          !this.cy
+            .style()
+            .json()
+            .find((el: any) => el.selector === '.green')
+        ) {
+          this.cy
+            .style()
+            .selector(`.green`)
+            .style({
+              'background-color': 'green',
+            })
+            .update();
         }
 
+        this.cy.getElementById(id).classes('green');
+        break;
 
-        this.openModal = false;
+      case 'blue':
+        if (
+          !this.cy
+            .style()
+            .json()
+            .find((el: any) => el.selector === '.blue')
+        ) {
+          this.cy
+            .style()
+            .selector(`.blue`)
+            .style({
+              'background-color': 'blue',
+            })
+            .update();
+        }
+
+        this.cy.getElementById(id).classes('blue');
+        break;
+
+      case 'red':
+        if (
+          !this.cy
+            .style()
+            .json()
+            .find((el: any) => el.selector === '.red')
+        ) {
+          this.cy
+            .style()
+            .selector(`.red`)
+            .style({
+              'background-color': 'red',
+            })
+            .update();
+        }
+
+        this.cy.getElementById(id).classes('red');
+        break;
+
+      case 'amber':
+        if (
+          !this.cy
+            .style()
+            .json()
+            .find((el: any) => el.selector === '.amber')
+        ) {
+          this.cy
+            .style()
+            .selector(`.amber`)
+            .style({
+              'background-color': 'amber',
+            })
+            .update();
+        }
+
+        this.cy.getElementById(id).classes('amber');
+        break;
+
+        case 'pink':
+            if (
+              !this.cy
+                .style()
+                .json()
+                .find((el: any) => el.selector === '.pink')
+            ) {
+              this.cy
+                .style()
+                .selector(`.pink`)
+                .style({
+                  'background-color': 'pink',
+                })
+                .update();
+            }
+    
+            this.cy.getElementById(id).classes('pink');
+            break;
+
+      case 'white':
+        this.cy.getElementById(id).classes('');
+        break;
     }
 
-    public saveGraph() {
-        console.log( this.cy.json() );
-    }
+    this.openModal = false;
+  }
 
-    public removeSelected(evt: any) {
-            evt.target.toggleClass('selected');
-            this.cy.$('.selected').remove();
-            this.closePreview();
-    }
+  public saveGraph() {
+    console.log(this.cy.json());
+  }
 
-    public addNode(x: number, y: number, label: string, info: any, type: string) {
-        this.cy.add({
-            group: 'nodes',
-            data: {
-                id: this.id,
-                type: type,
-                info: info,
-                label: label,
-            },
-            position: {x: x, y: y},
-        });
-    }
+  public removeSelected(evt: any) {
+    evt.target.toggleClass('selected');
+    this.cy.$('.selected').remove();
+    this.closePreview();
+  }
+
+  public addNode(x: number, y: number, label: string, info: any, type: string) {
+    this.cy.add({
+      group: 'nodes',
+      data: {
+        id: this.id,
+        type: type,
+        info: info,
+        label: label,
+      },
+      position: { x: x, y: y },
+    });
+  }
 }
